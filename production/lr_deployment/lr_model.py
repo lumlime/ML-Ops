ba024e9803cb77fe2a604f89e3b5f96ad4b3a665
Olupelumi Moses Kayode <Kayode-OM@ulster.ac.uk>
2024-12-09 05:03:09 +0000

    Update argument names in lr model

--- production/lr_deployment/lr_model.py
+++ production/lr_deployment/lr_model.py
@@ -15,8 +15,8 @@ mlflow.autolog()
 
 
 # Load training and test data
-train_df = pd.read_csv('args.trainingdata')
-test_df = pd.read_csv('args.testdata')
+train_df = pd.read_csv('args.training_data')
+test_df = pd.read_csv('args.test_data')
 
 # 'Activity' is the column with class labels
 Y_train = train_df['Activity'].values
